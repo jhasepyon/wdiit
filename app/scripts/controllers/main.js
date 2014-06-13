d3753820c54a3aee4e2244fc09f37facab138c16
hasegawa <jun-hasegawa@cybozu.co.jp>
2014-06-13 23:41:13 +0900

    yo angular-requireで雛形を作成

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+define(['angular'], function (angular) {
+  'use strict';
+
+  angular.module('wdiitApp.controllers.MainCtrl', [])
+    .controller('MainCtrl', function ($scope) {
+      $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+    });
+});
