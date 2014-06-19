0830d7b69fd1a95fd282169d29eb8d48d4e76d47
hasegawa <jun-hasegawa@cybozu.co.jp>
2014-06-19 22:57:39 +0900

    今日のトピックで表示する項目を絞った

--- app/scripts/controllers/today.js
+++ app/scripts/controllers/today.js
@@ -4,5 +4,10 @@ define(['angular'], function(angular) {
   angular.module('wdiitApp.controllers.TodayCtrl', [])
     .controller('TodayCtrl', function($scope, Today) {
       $scope.today = Today;
+
+      $scope.show = function(index, totalSize, showSize) {
+        var hoge = index % (totalSize / showSize);
+        return parseInt(hoge, 10) === 1;
+      };
     });
 });
